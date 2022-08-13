e785929f6450b9acae211e96b82dbf2251dcb62d
Keisuke Uemoto <keisuke.u211016@gmail.com>
2022-08-13 22:17:14 +0900

    イベント編集、更新/過去のイベントを分ける

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -18,12 +18,17 @@ class EventController extends Controller
      */
     public function index()
     {
+        $today = Carbon::today();
+
         $events = DB::table('events')
+            ->whereDate('start_date','>=',$today)
             ->orderBy('start_date', 'asc')
             ->paginate(10);
 
-        return view('manager.events.index',
-        compact('events'));
+        return view(
+            'manager.events.index',
+            compact('events')
+        );
     }
 
     /**
@@ -44,14 +49,14 @@ class EventController extends Controller
      */
     public function store(StoreEventRequest $request)
     {
-        $check = EventService::checkEventDuplication($request['event_date'],$request['start_time'],$request['end_time']);
-        if($check){
-            session()->flash('status','この時間帯は既に他の予約が存在します');
+        $check = EventService::checkEventDuplication($request['event_date'], $request['start_time'], $request['end_time']);
+        if ($check) {
+            session()->flash('status', 'この時間帯は既に他の予約が存在します');
             return view('manager.events.create');
         }
 
-        $startDate = EventService::joinDateAndTime($request['event_date'],$request['start_time']);
-        $endDate = EventService::joinDateAndTime($request['event_date'],$request['end_time']);
+        $startDate = EventService::joinDateAndTime($request['event_date'], $request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'], $request['end_time']);
 
         Event::create([
             'name' => $request['event_name'],
@@ -66,15 +71,14 @@ class EventController extends Controller
         return to_route('events.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function show(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+        return view('manager.events.show', compact('event', 'eventDate', 'startTime', 'endTime'));
     }
 
     /**
@@ -85,19 +89,40 @@ class EventController extends Controller
      */
     public function edit(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->editEventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+        return view('manager.events.edit', compact('event', 'eventDate', 'startTime', 'endTime'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateEventRequest  $request
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateEventRequest $request, Event $event)
     {
-        //
+        $check = EventService::countEventDuplication($request['event_date'], $request['start_time'], $request['end_time']);
+        if ($check > 1) {
+            $event = Event::findOrFail($event->id);
+            $eventDate = $event->editEventDate;
+            $startTime = $event->startTime;
+            $endTime = $event->endTime;
+            session()->flash('status', 'この時間帯は既に他の予約が存在します');
+            return view('manager.events.edit',compact('event', 'eventDate', 'startTime', 'endTime'));
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'], $request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'], $request['end_time']);
+
+        $event = Event::findOrFail($event->id);
+        $event->name = $request['event_name'];
+        $event->information = $request['information'];
+        $event->start_date = $startDate;
+        $event->end_date = $endDate;
+        $event->max_people = $request['max_people'];
+        $event->is_visible = $request['is_visible'];
+        $event->save();
+
+        session()->flash('status', '更新しました');
+        return to_route('events.index');
     }
 
     /**
@@ -109,5 +134,15 @@ class EventController extends Controller
     public function destroy(Event $event)
     {
         //
+    }
+
+    public function past()
+    {
+        $today = Carbon::today();
+        $events = DB::table('events')
+            ->whereDate('start_date', '<', $today)
+            ->orderBy('start_date', 'desc')
+            ->paginate(10);
+        return view('manager.events.past',compact('events'));
     }
 }
